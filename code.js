169717138ee6a2ef78778737f5392eb85eacc1ed
Abigail Trapp <abbsthepandahead@gmail.com>
2022-03-13 22:57:46 -0600

    html page setup

--- code.js
+++ code.js
@@ -12,12 +12,12 @@ else if(Math.ceil(i/5)*5 - i < 3){
 }
   return result
 }
-console.log(gradingStudents(88))
+console.log(gradingStudents([88]))
 
 
-// let grade = prompt("Enter the student's grade")
+let body = document.querySelector('body')
+let p = document.createElement('p')
+p.innerHTML= `${gradingStudents([88])}`
+body.append(p)
 
-// let body = document.querySelector("body")
-// let p = document.createElement("p")
-// p.innerHTML = `${diagonalDifference(grade)}`
-// body.append(p)
+
